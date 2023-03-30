37a100bdcd116ac2a5e003e6ef0d45f3a66d30d2
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-03-30 09:39:00 -0500

    Changed rofi theme to catppuccin-mocha (basic)

--- home-modules/rofi.nix
+++ home-modules/rofi.nix
@@ -4,8 +4,26 @@
 		enable = true;
 	
 		terminal = "kitty";
-		theme = "sidebar";
+		theme = "./catppuccin-mocha.rasi";
 
 		plugins = with pkgs; [ rofi-emoji rofi-calc ];
+		
+		extraConfig = {
+			modi = "run,drun,window";
+			icon-theme = "Oranchelo";
+	    show-icons = true;
+	    terminal = "alacritty";
+	    drun-display-format = "{icon} {name}";
+	    location = 0;
+	    disable-history = false;
+	    hide-scrollbar = true;
+	    display-drun = "   Apps ";
+	    display-run = "   Run ";
+	    display-window = " 﩯  Window";
+	    display-Network = " 󰤨  Network";
+	    sidebar-mode = true;
+		};
 	};
+
+	home.file.".config/rofi/catppuccin-mocha.rasi".source = ./rofi/catppuccin-mocha.rasi;
 }
