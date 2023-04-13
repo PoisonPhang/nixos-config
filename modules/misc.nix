f2c63c23788fb83c35f27adb6fbc89f67696f895
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-04-13 10:36:50 -0500

    Set time to America/Chicago

--- modules/misc.nix
+++ modules/misc.nix
@@ -22,7 +22,7 @@
     pam.services.lightdm.enableGnomeKeyring = true;
   };
 
-  time.timeZone = "Portugal";
+  time.timeZone = "America/Chicago";
   i18n.defaultLocale = "en_US.UTF-8";
 
   services = {
