23289abd486690cbbf230c75e03fece7a1ca1eeb
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-05-29 10:23:38 -0500

    chore: remove kitty multiplexing shortcuts

--- home-modules/kitty.nix
+++ home-modules/kitty.nix
@@ -16,23 +16,6 @@
       size = 16;
     };
 
-    keybindings = {
-      "kitty_mod+n" = "new_window_with_cwd";
-      "super+shift+enter" = "new_os_window_with_cwd";
-      "alt+enter" = "new_window_with_cwd";
-      "alt+w" = "close_window";
-      "alt+j" = "next_window";
-      "alt+t" = "new_tab";
-      "alt+]" = "next_tab";
-      "alt+[" = "previous_tab";
-      "alt+k" = "previous_window";
-      "alt+shift+k" = "move_window_backward";
-      "alt+r" = "start_resizing_window";
-      "alt+shift+r" = "set_tab_title";
-      "alt+p" = "next_layout";
-      "alt+shift+p" = "last_used_layout";
-    };
-
     theme = "Catppuccin-Mocha";
 
     extraConfig = ''
