d86b22de39e5d2153677e0b5720fabcd5d395a84
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2023-06-28 12:00:50 -0500

    fix: use stable until nvidia screen flickering is resolved

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
