ac3c50c0571f6fefb4184decebc05a84d7441892
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2025-01-20 13:27:19 -0600

    chore: configure ghostty

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -6,7 +6,9 @@
     settings = {
       theme = "catppuccin-mocha";
       font-family = "Comic Code Ligatures";
+      font-size = 14;
       window-decoration = false;
+      mouse-scroll-multiplier = 0;
     };
   };
 }
