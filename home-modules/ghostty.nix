742cd2068c27cdbfa2c59e00c93b00edb233ce83
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2025-09-23 10:51:40 -0500

    chore(ghostty): unstable

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs-unstable, ... }:
 {
   programs.ghostty = {
     enable = true;
+    package = pkgs-unstable.ghostty;
 
     settings = {
-      theme = "catppuccin-mocha";
+      theme = "Catppuccin Mocha";
       font-family = "Comic Code Ligatures";
       font-size = 14;
       window-decoration = false;
