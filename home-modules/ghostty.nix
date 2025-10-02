972abdaa212c98d24493cd081438fe414e133407
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2025-10-02 13:56:56 -0500

    fix(ghostty): enable scroll

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -9,7 +9,7 @@
       font-family = "Comic Code Ligatures";
       font-size = 14;
       window-decoration = false;
-      mouse-scroll-multiplier = 0;
+      mouse-scroll-multiplier = 3;
     };
   };
 }
